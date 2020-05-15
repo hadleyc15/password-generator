ebbae1479947df4e04c77f86114ec65f00e6f6bb
Chris Hadley <hadleyc15@yahoo.com>
2020-05-14 21:22:13 -0600

    added acceptable parameters:

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,10 @@
-// Assignment code here
+// Acceptable Parameters for each portion of password.
+var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChar = "1234567890";
+var specialChar = "!#$%&'()*"
 
-  
+
   // Get references to the #generate element
   var generateBtn = document.querySelector("#generate");
   
