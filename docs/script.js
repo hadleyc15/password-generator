455cc1bdc61851f0bae22234d0e2bef2291fb8e9
Chris Hadley <hadleyc15@yahoo.com>
2020-05-16 11:33:18 -0600

    added for loop and first prompt

--- docs/script.js
+++ docs/script.js
@@ -1,21 +1,42 @@
-// Acceptable Parameters for each portion of password.
-var lowerChar = "abcdefghijklmnopqrstuvwxyz";
-var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numberChar = "1234567890";
-var specialChar = "!#$%&'()*"
+// Assignment Code
+var Characters;
+
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var special = ["!", "@", "#","$", "%", "^", "&", "*", "~", "?"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var base = [];
+
+var buttonPassword = function () {
+
+  Characters = prompt("How many characters would you like your password to be?");
+
+  while (isNaN(Characters) || Characters < 8 || Characters > 128) Characters = Number(
+    prompt("You password must contain 8-128 characters...How many characters would you like in your password?"));
 
 
-  // Get references to the #generate element
-  var generateBtn = document.querySelector("#generate");
-  
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-  
+// Write password to the #password input
+var passwordButton = document.querySelector("password");
+var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", buttonPassword);
+generateBtn.addEventListener("click", writePassword);
+
+//Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+
+  function generatePassword() {
+    var randomPassword = "";
+    for (var i = 0; i < Characters; i++) {
+      randomPassword += base[Math.floor(Math.random() * base.length)]; 
+    }
+    return (randomPassword);
   }
-  
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+}
+}
